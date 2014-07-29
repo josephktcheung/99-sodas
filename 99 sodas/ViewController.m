4203383ad52005666db85ce8aaadd160ead56d8e
josephktcheung <josephktcheung@gmail.com>
2014-07-29 15:00:16 +0800

    Print 99 sodas on the wall in console log

--- 99 sodas/ViewController.m
+++ 99 sodas/ViewController.m
@@ -17,6 +17,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    for (int count = 99; count > 0; count --) {
+        if (count > 1) {
+            NSLog(@"%i bottles of beer on the wall, %i bottles of beer. Take one down and pass it around, %i bottles of beer on the wall.", count, count, count - 1);
+        }
+        else if (count == 1) {
+            NSLog(@"1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.");
+            NSLog(@"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning {
