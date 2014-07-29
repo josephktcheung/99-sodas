918331c4501850750bd97e06f2866e56c8e2c0f6
josephktcheung <josephktcheung@gmail.com>
2014-07-29 14:40:30 +0800

    Initial Commit

---
+++ 99 sodas/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  99 sodas
+//
+//  Created by Joseph Cheung on 29/7/14.
+//  Copyright (c) 2014 Reque.st. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
